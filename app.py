4f6cd0dfec76a0dafe1b3f5a80f43223c06f068a
Sakditach Thanasodsai <lemascserviceth@hotmail.com>
2024-10-17 06:52:09 +0000

    feat: recommend menu logic and wip result page

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import food
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index_page():
     return render_template("index.html")
 
+
 @app.route("/calculate")
 def calculate_page():
     return render_template("calculate.html")
+
 
 class CalculateForm(TypedDict):
     activity: str
@@ -19,17 +22,28 @@ class CalculateForm(TypedDict):
     disease: str
     name: str
     weight: str
-    
+
+
 @app.route("/result", methods=["POST"])
 def result_page():
     data: CalculateForm = request.form
-    sites = food.random_food()
-    return render_template("result.html", sites=sites)
+    min_protein, max_protein = food.calculate_protein(
+        float(data["weight"]),
+        int(data["age"]),
+        data["activity"],
+        data["disease"]
+    )
+    protein = (min_protein + max_protein) / 2
+    menu = food.random_food_in_range(min_protein, max_protein, data["allergy"])
+    print(menu)
+    return render_template("result.html", protein=f"{protein:.2f}", menu=menu, form=data)
+
 
 @app.route("/data")
 def data_page():
     food.get_df()
     return "Data cached at " + str(food.fetch_at)
+
 
 @app.route("/purge")
 def purge_page():
--- food.py
+++ food.py
@@ -1,3 +1,4 @@
+from typing import TypedDict, Literal
 import pandas as pd
 import random
 import time
@@ -6,10 +7,12 @@ df = None
 url = 'https://docs.google.com/spreadsheets/d/1_9AZb7hF7494ZyhKx-bTCcLHByKm0uXBq_pBInU1E84/export?gid=191160001&format=csv'
 fetch_at = 0
 
-def split_to_list(string):
-  return [x.strip() for x in string.split(",")]
 
-def get_df(use_cached = True):
+def split_to_list(string):
+    return [x.strip() for x in string.split(",")]
+
+
+def get_df(use_cached=True):
     global df, fetch_at
     if use_cached and df is not None:
         return df
@@ -17,9 +20,100 @@ def get_df(use_cached = True):
     fetch_at = time.time()
     return df
 
-def random_food():
+
+class FoodData(TypedDict):
+    menu: str
+    type: Literal["main", "dessert"]
+    ingredients: list[str]
+    protein_per_serving: float
+
+# สุ่มรายการอาหาร 1 รายการ จาก list ทั้งหมด
+
+
+def get_random_food(food_type):
     df = get_df()
-    num_of_rows = df.shape[0]
-    random_index = random.randint(0, num_of_rows - 1)
-    data_at_index = df.iloc[random_index]
-    return data_at_index.to_dict()
+    # สุ่ม index ในช่วงที่ต้องการ
+    dest = df[df["type"] == food_type]
+    random_index = random.randint(0, dest.shape[0] - 1)
+    data_at_index = dest.iloc[random_index]
+    data_dict: FoodData = data_at_index.to_dict()
+    return data_dict
+
+
+def calculate_protein_from_disease(disease):
+    min_protein = 1
+    max_protein = 1
+    if disease == "ไตวายเรื้อรัง":
+        min_protein = 0.6
+        max_protein = 0.6
+    elif disease in ("ตับ", "มะเร็ง", "ไฟไหม้กดทับ"):
+        min_protein = 1.2
+        max_protein = 2
+
+    return min_protein, max_protein
+
+
+def calculate_protein_from_age(age):
+    min_protein = 1
+    max_protein = 1
+    if age < 1:
+        min_protein = 1.56
+        max_protein = 1.56
+    elif age <= 18:
+        min_protein = 1.05
+        max_protein = 1.2
+    elif age >= 60:
+        min_protein = 1
+        max_protein = 1.2
+
+    return min_protein, max_protein
+
+
+def calculate_protein(weight, age, activity=None, disease=None):
+    min_protein, max_protein = calculate_protein_from_disease(disease)
+    if min_protein == 1 and max_protein == 1:
+        min_protein, max_protein = calculate_protein_from_age(age)
+
+    if activity == "ออกกำลังกาย":
+        min_protein += 0.4
+        max_protein += 0.8
+    elif activity == "นักเพาะกาย":
+        min_protein += 0.8
+        max_protein += 1.2
+    elif activity == "ลดน้ำหนัก":
+        min_protein -= 0.2
+        max_protein -= 0.2
+    else:
+        min_protein += 0.1
+        max_protein += 0.4
+
+    # multiple by weight
+    min_protein *= weight
+    max_protein *= weight
+
+    return min_protein, max_protein
+
+
+def random_food_in_range(min_protein, max_protein, allergy=None):
+    recommend_menu: list[FoodData] = []
+    menu_names = set()
+    total_protein = 0
+    current_type = "main"
+    random_attempt = 0
+    while total_protein < min_protein and random_attempt < 2:
+        random_attempt += 1
+        random_food = get_random_food(current_type)
+        menu_name = random_food["menu"]
+        if allergy and allergy in "".join(random_food["ingredients"]):
+            continue
+        total_after_add = total_protein + random_food["protein_per_serving"]
+        if not menu_name in menu_names and total_after_add <= max_protein:
+            recommend_menu.append(random_food)
+            menu_names.add(menu_name)
+            total_protein = total_after_add
+            random_attempt = 0
+            if current_type == "main":
+                current_type = "dessert"
+            else:
+                current_type = "main"
+    return recommend_menu
